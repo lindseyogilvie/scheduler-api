3ec18c8f147d60feed50ee72409997a606f73c6a
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-03-14 14:19:37 -0700

    Fixes create appointment bug

--- src/routes/appointments.js
+++ src/routes/appointments.js
@@ -32,7 +32,7 @@ module.exports = (db, updateAppointment) => {
       return;
     }
 
-    const { student, interviewer } = request.body;
+    const { student, interviewer } = request.body.interview;
 
     db.query(
       `
@@ -45,7 +45,7 @@ module.exports = (db, updateAppointment) => {
       .then(() => {
         setTimeout(() => {
           response.status(204).json({});
-          updateAppointment(Number(request.params.id), request.body);
+          updateAppointment(Number(request.params.id), request.body.interview);
         }, 1000);
       })
       .catch(error => console.log(error));
